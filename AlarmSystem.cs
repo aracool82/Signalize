da3f58d0453a65be2e267d2cdcfa226f46a477d1
aracool82 <aracool1982@gmail.com>
2024-12-18 15:38:08 +0700

    ver_3_0

--- AlarmSystem.cs
+++ AlarmSystem.cs
@@ -3,58 +3,45 @@ using UnityEngine;
 
 public class AlarmSystem : MonoBehaviour
 {
+    private const float MaxVolume = 1.0f;
+    private const float MinVolume = 0.0f;
+
     [SerializeField] private AudioSource _audio;
     [SerializeField] private float _fadeSpeed = 0.2f;
 
-    private float _maxVolume = 1.0f;
-    private float _minVolume = 0.0f;
-
-    private Coroutine _coroutineFadeIn;
-    private Coroutine _coroutineFadeOut;
+    private Coroutine _coroutine;
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.TryGetComponent(out Thief thief))
-        {
-            if (_coroutineFadeOut != null)
-            {
-                StopCoroutine(_coroutineFadeOut);
-                _coroutineFadeOut = null;
-            }
-
-            _coroutineFadeIn ??= StartCoroutine(FadeIn());
-            _audio.Play();
-        }
+        if (!collider.TryGetComponent(out Thief thief))
+            return;
+        
+        StartCoroutine(MaxVolume);
+        _audio.Play();
     }
-
+    
     private void OnTriggerExit(Collider collider)
     {
         if (collider.TryGetComponent(out Thief thief))
-        {
-            if (_coroutineFadeIn != null)
-            {
-                StopCoroutine(_coroutineFadeIn);
-                _coroutineFadeIn = null;
-            }
-
-            _coroutineFadeOut ??= StartCoroutine(FadeOut());
-        }
+            StartCoroutine(MinVolume);
     }
 
-    private IEnumerator FadeIn()
+    private void StartCoroutine(float volume)
     {
-        while (_audio.volume <= _maxVolume)
+        if (_coroutine != null)
         {
-            _audio.volume = Mathf.MoveTowards(_audio.volume, _maxVolume, _fadeSpeed * Time.deltaTime);
-            yield return null;
+            StopCoroutine(_coroutine);
+            _coroutine = null;
         }
+
+        _coroutine = StartCoroutine(ChangeVolume(volume));
     }
 
-    private IEnumerator FadeOut()
+    private IEnumerator ChangeVolume(float volume)
     {
-        while (_audio.volume >= _minVolume)
+        while (true)
         {
-            _audio.volume = Mathf.MoveTowards(_audio.volume, _minVolume, _fadeSpeed * Time.deltaTime);
+            _audio.volume = Mathf.MoveTowards(_audio.volume, volume, _fadeSpeed * Time.deltaTime);
             yield return null;
         }
     }
